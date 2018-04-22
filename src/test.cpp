fbe240de8481666fa130eee0f70d7b717871d2a6
Malegkin <malegkin@gmail.com>
2018-04-22 15:31:28 +0300

    travis xxx

--- src/test.cpp
+++ src/test.cpp
@@ -27,7 +27,7 @@ BOOST_AUTO_TEST_SUITE( test_suite_main )
             ol.push_back( i );
         }
 
-        uint32_t acc = accumulate(ol.begin(), ol.end(), uint32_t(0), [](uint32_t sum, auto a){ return sum + static_cast<uint32_t>(a);} );
+        uint32_t acc = accumulate(ol.begin(), ol.end(), uint32_t(0), [](uint32_t sum, uint32_t a){ return sum + static_cast<uint32_t>(a);} );
 
         BOOST_REQUIRE(acc == (1 + 1000)*500 );
     }
@@ -40,7 +40,7 @@ BOOST_AUTO_TEST_SUITE( test_suite_main )
             ol.push_back( i );
         }
 
-        uint32_t acc = accumulate(ol.begin(), ol.end(), uint32_t(0), [](uint32_t sum, auto a){ return sum + static_cast<uint32_t>(a);} );
+        uint32_t acc = accumulate(ol.begin(), ol.end(), uint32_t(0), [](uint32_t sum, uint32_t a){ return sum + static_cast<uint32_t>(a);} );
 
         BOOST_REQUIRE(acc == (1 + 1000)*500 );
     }
